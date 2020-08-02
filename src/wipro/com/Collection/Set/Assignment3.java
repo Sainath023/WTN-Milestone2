38f15b2f044e6e91c48e4eb8a3924e0f5190e911
ASUS <ASUS@DESKTOP-5FTP1OJ>
2020-08-03 00:12:02 +0530

    Intial commit

--- src/wipro/com/Collection/Set/Assignment3.java
+++ src/wipro/com/Collection/Set/Assignment3.java
@@ -1,5 +1,37 @@
 package wipro.com.Collection.Set;
+import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
 
-public class Assignment3 {
-
+public class Assignment3
+{
+	public static void main(String[] args) 
+	{
+        TreeSet<String> t=new TreeSet<>(Collections.reverseOrder());
+        t.add("Ram");
+        t.add("Abhi");
+        t.add("Sai");
+        t.add("Revanth");
+        t.add("Ravi");
+        Iterator<String> i=t.iterator();
+        String name="Sai";
+        boolean result=false;
+        while(i.hasNext())
+        {
+        	if(i.next().equals(name))
+        	{
+        		result=true;
+        		break;
+        	}
+        }
+        if(result)
+        {
+        	System.out.println(name +" : Exsists");
+        }
+        else
+        {
+        	System.out.println(name +" : Does not Exsists");
+        }
+	}
 }
